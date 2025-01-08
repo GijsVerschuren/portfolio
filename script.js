8d86e606f328c58b2bc2ae745df05c2cb6e16787
GijsVerschuren <153927139+GijsVerschuren@users.noreply.github.com>
2025-01-08 10:50:37 +0100

    projects text
    added the same as i did for the home button but to a new one for the projects page. wanted to do all in one commit but this took way longer then expected

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//show home text
 function HomeText() {
     const oneDiv = document.getElementById('one');
     const textDiv = document.querySelector('.text');
@@ -19,3 +20,26 @@ function HomeText() {
 
 document.addEventListener('DOMContentLoaded', HomeText);
 
+
+
+//show project text
+function projectText() {
+    const twoDiv = document.getElementById('two');
+    const projectDiv = document.querySelector('.projects');
+    const svgElement = projectDiv.querySelector('svg');
+    const textContent = projectDiv.innerHTML.replace(svgElement.outerHTML, '');
+    projectDiv.innerHTML = '';
+    projectDiv.appendChild(svgElement);
+
+    twoDiv.addEventListener('mouseover', () => {
+        projectDiv.classList.add("show");
+
+    });
+
+    twoDiv.addEventListener('mouseout', () => {
+        projectDiv.classList.remove("show");
+
+    });
+}
+
+document.addEventListener('DOMContentLoaded', projectText);
